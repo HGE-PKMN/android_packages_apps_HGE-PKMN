f3e96711265f9f57803557bb684cce94430b0975
Myself5 <thchod@gmail.com>
2015-05-06 17:03:07 +0200

    Add Movement Controls and Maps
    Credits:
    WID2 @blackbread
    pseudo & basic_asp @D0mm4S

--- app/src/main/java/de/hg_epp/whereisdon/TMXTiledMap.java
+++ app/src/main/java/de/hg_epp/whereisdon/TMXTiledMap.java
@@ -3,15 +3,14 @@ package de.hg_epp.whereisdon;
 import java.io.IOException;
 
 import org.andengine.engine.camera.BoundCamera;
+import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl;
+import org.andengine.engine.camera.hud.controls.BaseOnScreenControl;
+import org.andengine.engine.camera.hud.controls.DigitalOnScreenControl;
 import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
-import org.andengine.entity.IEntity;
-import org.andengine.entity.modifier.LoopEntityModifier;
-import org.andengine.entity.modifier.PathModifier;
-import org.andengine.entity.modifier.PathModifier.IPathModifierListener;
-import org.andengine.entity.modifier.PathModifier.Path;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.AnimatedSprite;
@@ -25,15 +24,18 @@ import org.andengine.extension.tmx.TMXTileProperty;
 import org.andengine.extension.tmx.util.exception.TMXLoadException;
 import org.andengine.opengl.texture.ITexture;
 import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.bitmap.AssetBitmapTexture;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TextureRegionFactory;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.Constants;
 import org.andengine.util.debug.Debug;
 
+import android.opengl.GLES20;
 import android.view.View;
-import android.widget.Toast;
 
 /**
  * Based Off TMXTiledMapExample.java by
@@ -44,7 +46,7 @@ import android.widget.Toast;
  * using Google Non Sticky Immersive Mode
  * @author Christian Oder
  */
-public class TMXTiledMap extends SimpleBaseGameActivity {
+public class TMXTiledMap extends SimpleBaseGameActivity  {
 
     // Non Sticky Immersive Mode
     @Override
@@ -78,6 +80,14 @@ public class TMXTiledMap extends SimpleBaseGameActivity {
 
     private org.andengine.extension.tmx.TMXTiledMap mTMXTiledMap;
 
+    private BitmapTextureAtlas mOnScreenControlTexture;
+    private ITextureRegion mOnScreenControlBaseTextureRegion;
+    private ITextureRegion mOnScreenControlKnobTextureRegion;
+
+    static AnimatedSprite mPlayer;
+
+    private DigitalOnScreenControl mDigitalOnScreenControl;
+
     // ===========================================================
     // Constructors
     // ===========================================================
@@ -103,6 +113,11 @@ public class TMXTiledMap extends SimpleBaseGameActivity {
         this.mPlayerTexture = new AssetBitmapTexture(this.getTextureManager(), this.getAssets(), "gfx/player.png", TextureOptions.DEFAULT);
         this.mPlayerTextureRegion = TextureRegionFactory.extractTiledFromTexture(this.mPlayerTexture, 3, 4);
         this.mPlayerTexture.load();
+
+        this.mOnScreenControlTexture = new BitmapTextureAtlas(this.getTextureManager(), 256, 128, TextureOptions.BILINEAR);
+        this.mOnScreenControlBaseTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mOnScreenControlTexture, this, "gfx/onscreen_control_base.png", 0, 0);
+        this.mOnScreenControlKnobTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mOnScreenControlTexture, this, "gfx/onscreen_control_knob.png", 128, 0);
+        this.mOnScreenControlTexture.load();
     }
 
     @Override
@@ -117,7 +132,7 @@ public class TMXTiledMap extends SimpleBaseGameActivity {
                 public void onTMXTileWithPropertiesCreated(final org.andengine.extension.tmx.TMXTiledMap pTMXTiledMap, final TMXLayer pTMXLayer, final TMXTile pTMXTile, final TMXProperties<TMXTileProperty> pTMXTileProperties) {
                 }
             });
-            this.mTMXTiledMap = tmxLoader.loadFromAsset("tmx/WIDmap.tmx");
+            this.mTMXTiledMap = tmxLoader.loadFromAsset("tmx/basic_asp.tmx");
             this.mTMXTiledMap.setOffsetCenter(0, 0);
         } catch (final TMXLoadException e) {
             Debug.e(e);
@@ -134,46 +149,32 @@ public class TMXTiledMap extends SimpleBaseGameActivity {
         final float centerY = CAMERA_HEIGHT / 2;
 
 		/* Create the sprite and add it to the scene. */
-        final AnimatedSprite player = new AnimatedSprite(centerX, centerY, this.mPlayerTextureRegion, this.getVertexBufferObjectManager());
-        player.setOffsetCenterY(0);
-        this.mCamera.setChaseEntity(player);
+        mPlayer = new AnimatedSprite(centerX, centerY, this.mPlayerTextureRegion, this.getVertexBufferObjectManager());
+        mPlayer.setOffsetCenterY(0);
+        this.mCamera.setChaseEntity(mPlayer);
+        final PhysicsHandler physicsHandler = new PhysicsHandler(mPlayer);
+        mPlayer.registerUpdateHandler(physicsHandler);
 
-        final Path path = new Path(5).to(50, 740).to(50, 1000).to(820, 1000).to(820, 740).to(0);
-
-        player.registerEntityModifier(new LoopEntityModifier(new PathModifier(30, path, null, new IPathModifierListener() {
+        		/* Velocity control (left). */
+        // Amount of Pixels away from 0/0 (top left corner)
+        final float edge_space = 40;
+        final AnalogOnScreenControl velocityOnScreenControl = new AnalogOnScreenControl(edge_space, edge_space, this.mCamera, this.mOnScreenControlBaseTextureRegion, this.mOnScreenControlKnobTextureRegion, 0.1f, this.getVertexBufferObjectManager(), new AnalogOnScreenControl.IAnalogOnScreenControlListener() {
             @Override
-            public void onPathStarted(final PathModifier pPathModifier, final IEntity pEntity) {
+            public void onControlChange(final BaseOnScreenControl pBaseOnScreenControl, final float pValueX, final float pValueY) {
 
+                physicsHandler.setVelocity(pValueX * 100, pValueY * 100);
             }
 
             @Override
-            public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
-                switch(pWaypointIndex) {
-                    case 0:
-                        player.animate(new long[] { 200, 200, 200 }, 0, 2, true);
-                        break;
-                    case 1:
-                        player.animate(new long[]{200, 200, 200 }, 3, 5, true);
-                        break;
-                    case 2:
-                        player.animate(new long[] { 200, 200, 200 }, 6, 8, true);
-                        break;
-                    case 3:
-                        player.animate(new long[] { 200, 200, 200 }, 9, 11, true);
-                        break;
-                }
+            public void onControlClick(final AnalogOnScreenControl pAnalogOnScreenControl) {
+				/* Nothing. */
             }
+        });
+        velocityOnScreenControl.getControlBase().setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        velocityOnScreenControl.getControlBase().setAlpha(0.5f);
+        velocityOnScreenControl.getControlBase().setScale(0.5f);
 
-            @Override
-            public void onPathWaypointFinished(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
-
-            }
-
-            @Override
-            public void onPathFinished(final PathModifier pPathModifier, final IEntity pEntity) {
-
-            }
-        })));
+        scene.setChildScene(velocityOnScreenControl);
 
 		/* Now we are going to create a rectangle that will  always highlight the tile below the feet of the pEntity. */
         final Rectangle currentTileRectangle = new Rectangle(0, 0, this.mTMXTiledMap.getTileWidth(), this.mTMXTiledMap.getTileHeight(), this.getVertexBufferObjectManager());
@@ -187,22 +188,23 @@ public class TMXTiledMap extends SimpleBaseGameActivity {
 
         scene.registerUpdateHandler(new IUpdateHandler() {
             @Override
-            public void reset() { }
+            public void reset() {
+            }
 
             @Override
             public void onUpdate(final float pSecondsElapsed) {
 				/* Get the scene-coordinates of the players feet. */
-                final float[] playerFootCordinates = player.convertLocalCoordinatesToSceneCoordinates(16, 1);
+                final float[] playerFootCordinates = mPlayer.convertLocalCoordinatesToSceneCoordinates(16, 1);
 
 				/* Get the tile the feet of the player are currently waking on. */
                 final TMXTile tmxTile = tmxLayer.getTMXTileAt(playerFootCordinates[Constants.VERTEX_INDEX_X], playerFootCordinates[Constants.VERTEX_INDEX_Y]);
-                if(tmxTile != null) {
+                if (tmxTile != null) {
                     // tmxTile.setTextureRegion(null); <-- Eraser-style removing of tiles =D
                     currentTileRectangle.setPosition(tmxLayer.getTileX(tmxTile.getTileColumn()), tmxLayer.getTileY(tmxTile.getTileRow()));
                 }
             }
         });
-        scene.attachChild(player);
+        scene.attachChild(mPlayer);
 
         return scene;
     }
