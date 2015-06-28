d47eb5a6e21b267121707c7a5e0d6a7098ec9ebb
Myself5 <thchod@gmail.com>
2015-06-28 14:23:09 +0200

    Dim Navbar Buttons

--- app/src/main/java/de/hg_epp/whereisdon/FightEngine.java
+++ app/src/main/java/de/hg_epp/whereisdon/FightEngine.java
@@ -78,6 +78,7 @@ public class FightEngine extends ActionBarActivity implements Animation.Animatio
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LOW_PROFILE
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
--- app/src/main/java/de/hg_epp/whereisdon/TMXTiledMapDigital.java
+++ app/src/main/java/de/hg_epp/whereisdon/TMXTiledMapDigital.java
@@ -76,6 +76,7 @@ public class TMXTiledMapDigital extends SimpleBaseGameActivity  {
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LOW_PROFILE
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 
